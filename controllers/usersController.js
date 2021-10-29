32a117da6da022be4ed24e1a099557910a78a4f6
Jonathan Shine <jonathanhshine@gmail.com>
2021-10-29 10:27:34 +0200

    created users Router and Controller and added route to server.js

---
+++ controllers/usersController.js
@@ -0,0 +1,4 @@
+// IMPORTS ------------------------------------------
+import createError from 'http-errors';
+import User from '../models/User.js';
+// --------------------------------------------------
