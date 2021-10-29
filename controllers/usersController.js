c2c5bcf52166223276cc8641b1e36d3083defd02
Jonathan Shine <jonathanhshine@gmail.com>
2021-10-29 11:21:03 +0200

    added getAllUsers and createUser methods and connected to routes, tested successfully in Insomnia

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,3 +2,27 @@
 import createError from 'http-errors';
 import User from '../models/User.js';
 // --------------------------------------------------
+
+
+// METHODS ------------------------------------------
+export const createUser = async (req, res, next) => {
+  const data = req.body;
+
+  try {
+    const user = new User(data);
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (error) {
+    next(error);
+  };
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    next(error);
+  };
+};
+// --------------------------------------------------
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,4 +1,11 @@
 import express from 'express';
 const router = express.Router();
 
+import {
+  createUser,
+  getAllUsers
+} from '../controllers/usersController.js';
+
+router.route("/").get(getAllUsers).post(createUser);
+
 export default router;
